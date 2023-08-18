7046ca4e3260a49689b5f16b409f168efd3d417d
abhishekcgupta1 <abhishekcgupta1@gmail.com>
2023-08-19 00:03:12 +0530

    18August2023

---
+++ Python/Hacker_Rank/helloWorld.py
@@ -0,0 +1,15 @@
+import operator
+
+if __name__ == '__main__':
+    print("Hello, World!")
+
+def sum(a,b):
+    return a+b
+    
+if __name__ == "__main__":
+    print("hellos")
+    Number1 = float(input("Enter 1st Number : "))
+    Number2 = float(input("Enter 2nd Number : "))
+    Sum = sum(Number1, Number2) 
+    print("First Number {0} which is getting added to Second Number {1} giving sum {2}".format(Number1, Number2, Sum))
+    print(operator.add(Number1,Number2))
--- Python/selectionStatement-ifElseLadder.py
+++ Python/selectionStatement-ifElseLadder.py
@@ -1,10 +1,10 @@
 # python program to illustrate statements
 # selection statements
 
-num1 = 34 
-if (num1 > 30): 
+num1 = 34
+if (num1 > 30):
     print("number is greater than 30")
-elif (num2 > 35) :
+elif (num2 > 35):
     print("number is greater than 35")
-else :
+else:
     print("number is less than 30")
